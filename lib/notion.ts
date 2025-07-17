51fc36f676e7ef39ff1487cb915472da96724a29
chugue <chugue85@gmail.com>
2025-07-17 16:36:10 +0900

    fix: unused import remove

--- lib/notion.ts
+++ lib/notion.ts
@@ -3,7 +3,6 @@ import type { Post, TagFilterItem } from '@/types/blog';
 import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 import { NotionUser } from '@/types/notion';
 import { NotionToMarkdown } from 'notion-to-md';
-import { unstable_cache } from 'next/cache';
 
 export const notion = new Client({
   auth: process.env.NOTION_TOKEN,
