45f81f9dbda5b2cb9a906c9ce29d82acea9c71b9
chugue <chugue85@gmail.com>
2025-07-18 16:22:54 +0900

    fix: header title changed

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="bg-background sticky top-0 z-50 border-b">
       <div className="container flex h-[var(--header-height)] items-center px-4">
         <Link href="/" className="text-xl font-semibold">
-          <span className="font-bold">짐코딩 블로그</span>
+          <span className="font-bold">Stephen&apos;s 기술블로그</span>
         </Link>
         <nav className="ml-auto flex items-center gap-4">
           <ThemeToggle />
