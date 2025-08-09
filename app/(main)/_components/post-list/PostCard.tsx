ef60d64beb582045e5a7f7e752d6dbc4e21e4f13
chugue <chugue85@gmail.com>
2025-08-09 18:52:45 +0900

    save: 0809

--- app/(main)/_components/post-list/PostCard.tsx
+++ app/(main)/_components/post-list/PostCard.tsx
@@ -26,7 +26,7 @@ export function PostCard({ post, isFirst = false }: PostCardProps) {
       )}
 
       {/* ---------- CardContent ---------- */}
-      <CardContent className="relative my-0 flex flex-1 flex-col p-0">
+      <CardContent className="group-hover:text-primary relative my-0 flex flex-1 flex-col p-0 transition-all duration-300">
         {/* 기본 콘텐츠 (레이아웃 유지용) */}
         <div className="flex flex-col px-2 transition-opacity duration-300">
           <div className="mb-4 flex flex-wrap gap-2">
@@ -37,9 +37,9 @@ export function PostCard({ post, isFirst = false }: PostCardProps) {
 
           <div
             data-title={post.title}
-            className="after:bg-card after:text-primary relative -mx-2 px-2 after:pointer-events-none after:absolute after:inset-x-0 after:top-0 after:z-10 after:[display:block] after:rounded-md after:px-2 after:pb-2 after:text-xl after:font-bold after:tracking-tight after:whitespace-normal after:opacity-0 after:backdrop-blur-sm after:transition-all after:duration-300 after:content-[attr(data-title)] group-hover:after:opacity-100"
+            className="after:bg-card-post_list dark:after:bg-card relative -mx-2 overflow-visible px-2 after:pointer-events-none after:absolute after:inset-x-0 after:top-0 after:z-10 after:[display:block] after:px-2 after:pb-2 after:text-xl after:font-bold after:tracking-tight after:whitespace-normal after:opacity-0 after:transition-opacity after:duration-300 after:content-[attr(data-title)] group-hover:after:opacity-100"
           >
-            <h2 className="mb-2 line-clamp-2 min-h-[3.5rem] text-xl font-bold tracking-tight">
+            <h2 className="mb-2 line-clamp-2 min-h-[3.5rem] text-xl font-bold tracking-tight transition-opacity duration-300 group-hover:opacity-0">
               {post.title}
             </h2>
           </div>
