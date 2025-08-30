404f2060fd729d803bd3456090e39bf5f5d8a39a
chugue <chugue85@gmail.com>
2025-08-30 16:48:53 +0900

    fix: vercel bot check added

--- domain/utils/page-view.utils.ts
+++ domain/utils/page-view.utils.ts
@@ -1,6 +1,8 @@
 export const crawlingBotCheck = (userAgent: string) => {
   if (!userAgent) return false;
   const ua = userAgent.toLowerCase();
+
+  if (ua.startsWith('vercel-')) return true;
   const botRegex =
     /\b(?:googlebot|bingbot|yandexbot|duckduckbot|baiduspider|ahrefsbot|semrushbot|mj12bot|baiduspider)\b/;
   return botRegex.test(ua);
