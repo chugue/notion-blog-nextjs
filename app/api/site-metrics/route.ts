cc2feff6701acebdfdc201ea96f9b04a9e4f0045
chugue <chugue85@gmail.com>
2025-09-01 17:11:51 +0900

    fix: visit stat cache on server

--- app/api/site-metrics/route.ts
+++ app/api/site-metrics/route.ts
@@ -1,13 +1,23 @@
 import { diContainer } from '@/shared/di/di-container';
 import { MainPageChartData } from '@/shared/types/main-page-chartdata';
 import { Result } from '@/shared/types/result';
+import { unstable_cache } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request): Promise<NextResponse<Result<MainPageChartData[]>>> {
   try {
     const siteMetricUseCase = diContainer.siteMetric.siteMetricUsecase;
-    const data = await siteMetricUseCase.getThirtyDaysSiteMetrics();
-    return NextResponse.json({ success: true, data });
+    const cachedFn = unstable_cache(
+      async () => {
+        const data = await siteMetricUseCase.getThirtyDaysSiteMetrics();
+        return data;
+      },
+      ['site-metrics'],
+      { tags: ['site-metrics'], revalidate: 60 }
+    );
+
+    const data = await cachedFn();
+    return NextResponse.json({ success: true, data: data });
   } catch (error) {
     return NextResponse.json({
       success: false,
