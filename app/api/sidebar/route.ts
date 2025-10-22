b766824301c7febf22bb2f4e7decbf53fb135026
chugue <chugue85@gmail.com>
2025-10-22 19:17:16 +0900

    test: webhook-sidebar

--- app/api/sidebar/route.ts
+++ app/api/sidebar/route.ts
@@ -1,7 +1,26 @@
+import { createHmac, timingSafeEqual } from 'crypto';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
+  const verificationToken = process.env.NOTION_WEBHOOK_SECERT_TOKEN;
+
+  if (!verificationToken) {
+    console.error('Verification token is missing');
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+
   const body = await request.json();
+
+  const signature = `sha256=${createHmac('sha256', verificationToken).update(JSON.stringify(body)).digest('hex')}`;
+  const notionSignature = request.headers.get('X-Notion-Signature');
+
+  const isTrustedPayload =
+    signature && timingSafeEqual(Buffer.from(signature), Buffer.from(notionSignature || ''));
+
+  if (!isTrustedPayload) {
+    console.warn('Invalid signature - ignoring request');
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
 
   console.log(body);
 
